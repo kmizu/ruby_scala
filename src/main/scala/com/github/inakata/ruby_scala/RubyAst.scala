96ad90da2fb08ddbe5f2b6f91c62244268aeebe7
Kota Mizushima <mizukota@gmail.com>
2014-03-27 03:28:34 +0900

    Some AST nodes which describe literals

---
+++ src/main/scala/com/github/inakata/ruby_scala/RubyAst.scala
@@ -0,0 +1,15 @@
+package com.github.inakata.ruby_scala
+
+/**
+ * This object provide the namespace in which Ruby's AST node classes is included
+ */
+object RubyAst {
+  case class Position(line: Int, column: Int)
+  abstract class Node(position: Position)
+  case class RawStringLiteral(content: String, position: Position) extends Node(position)
+  case class FixnumLiteral(content: Int, position: Position) extends Node(position)
+  case class BignumLiteral(content: BigInt, position: Position) extends Node(position)
+  case class FloatLiteral(content: Float, position: Position) extends Node(position)
+  case class DoubleLiteral(content: Double, position: Position) extends Node(position)
+  case class BooleanLiteral(content: Boolean, position: Position) extends Node(position)
+}
